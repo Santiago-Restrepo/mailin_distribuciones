d0504b4d3dfa7d02f39af81b8b7c67b54173690c
Santiago Restrepo <santirstrp@gmail.com>
2021-02-17 13:19:56 -0500

    Se le añaden animaciones y perspectiva al slider de ofertas

--- js/glider.js
+++ js/glider.js
@@ -22,8 +22,8 @@ window.addEventListener('load', function(){
 			  // screens greater than >= 1024px
 			  breakpoint: 1024,
 			  settings: {
-				slidesToShow: 3,
-				slidesToScroll: 1
+				slidesToShow: 5,
+				slidesToScroll: 5
 			  }
 			}
 		]
