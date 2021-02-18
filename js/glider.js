cb92abf0dbd32b52798f85e4fa5f4596a3891824
Santiago Restrepo <santirstrp@gmail.com>
2021-02-18 18:18:21 -0500

    Terminadas todas las versiones del carousel de ofertas

--- js/glider.js
+++ js/glider.js
@@ -1,44 +1,46 @@
 const gliderElements = document.querySelectorAll('.offer');
+const gliderArrowLeft = document.querySelector('.glider__left');
+const gliderArrowRight = document.querySelector('.glider__right');
 
 
 window.addEventListener('load', function(){
 	new Glider(document.querySelector('.glider__list'), {
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		draggable: true,
+		draggable: false,
+		duration:2,
 		dots: '.glider__dots',
 		rewind: true,
 		arrows: {
 			prev: '.glider__left',
 			next: '.glider__right'
 		},
-		// responsive: [
-		// 	{
-		// 	  // screens greater than >= 775px
-		// 	  breakpoint: 768,
-		// 	  settings: {
-		// 		// Set to `auto` and provide item width to adjust to viewport
-		// 		slidesToShow: 3,
-		// 		slidesToScroll: 3
-		// 	  }
-		// 	},{
-		// 	  // screens greater than >= 1024px
-		// 	  breakpoint: 1024,
-		// 	  settings: {
-		// 		slidesToShow: 5,
-		// 		slidesToScroll: 5
-		// 	  }
-		// 	}
-		// ]
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 768,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+			}
+		},{
+			// screens greater than >= 1024px
+			breakpoint: 1024,
+			settings: {
+				duration:1,
+			  }
+			}
+		]
 	});
 });
 
 gliderElements.forEach(element => {
-	element.addEventListener('mouseover',stopScroll)
+	element.addEventListener('mouseover',stopScroll);
 });
-gliderElements.forEach(element => {
-	element.addEventListener('mouseout',stopScroll)
-});
+gliderArrowLeft.addEventListener('mouseover',stopScroll);
+gliderArrowRight.addEventListener('mouseover',stopScroll);
+
+
+
 
 function stopScroll (){
 	clearInterval(intervalScroll);
@@ -51,4 +53,3 @@ var intervalScroll = window.setInterval(scrollSlide,2500);
 function scrollSlide(){
 	document.querySelector(".glider__right").click();
 }
-
