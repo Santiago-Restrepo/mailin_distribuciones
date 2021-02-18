02f66907cf2f98a8d1c9862f627d8c4103247ca7
Santiago Restrepo <santirstrp@gmail.com>
2021-02-18 16:34:53 -0500

    Primer intento de responsive design en home

--- js/glider.js
+++ js/glider.js
@@ -12,24 +12,24 @@ window.addEventListener('load', function(){
 			prev: '.glider__left',
 			next: '.glider__right'
 		},
-		responsive: [
-			{
-			  // screens greater than >= 775px
-			  breakpoint: 700,
-			  settings: {
-				// Set to `auto` and provide item width to adjust to viewport
-				slidesToShow: 3,
-				slidesToScroll: 3
-			  }
-			},{
-			  // screens greater than >= 1024px
-			  breakpoint: 1024,
-			  settings: {
-				slidesToShow: 5,
-				slidesToScroll: 5
-			  }
-			}
-		]
+		// responsive: [
+		// 	{
+		// 	  // screens greater than >= 775px
+		// 	  breakpoint: 768,
+		// 	  settings: {
+		// 		// Set to `auto` and provide item width to adjust to viewport
+		// 		slidesToShow: 3,
+		// 		slidesToScroll: 3
+		// 	  }
+		// 	},{
+		// 	  // screens greater than >= 1024px
+		// 	  breakpoint: 1024,
+		// 	  settings: {
+		// 		slidesToShow: 5,
+		// 		slidesToScroll: 5
+		// 	  }
+		// 	}
+		// ]
 	});
 });
 
