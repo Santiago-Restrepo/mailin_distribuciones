44fd4a0554f79b3a8fde098e99a3c43b7c843e34
Santiago Restrepo <santirstrp@gmail.com>
2021-03-22 16:21:21 -0500

    Llamado a API y renderizado de elementos terminado, todo right

--- js/modal_index.js
+++ js/modal_index.js
@@ -1,17 +1,20 @@
 var PRODUCT_MODAL= document.querySelector(".productModal");
 
-const openOfferModal = async (e)=>{
+const openProductModal = async (e)=>{
     //Guardamos la oferta que fue seleccionada accediendo al abuelo de la imagen clickeada
     try {
-        let offer_selected= e.target.parentElement.parentElement;
-        let offer_number= offer_selected.classList[1];//obtención de número de hijo
-        let productPromise = await fetch(`${API}${offer_number}.json`);//llamado a la base de datos
+        let product_selected= e.target.parentElement.parentElement;
+        let product_number= product_selected.classList[1];//obtención de número de hijo
+        let productPromise = await fetch(`${API}${product_number}.json`);//llamado a la base de datos
         let product = await productPromise.json();//llamado a la base de datos
 
         PRODUCT_MODAL.childNodes[2].firstElementChild.setAttribute("src",product.images.modal);//colocación de imagen
         PRODUCT_MODAL.childNodes[4].childNodes[1].innerHTML= product.name;//colocación de nombre
         PRODUCT_MODAL.childNodes[4].childNodes[3].innerHTML= product.description;//colocación de descripción
-        PRODUCT_MODAL.childNodes[4].childNodes[5].firstElementChild.firstElementChild.innerHTML= product.prices.old;//colocación de precio antiguo
+        if(product.prices.old){
+            PRODUCT_MODAL.childNodes[4].childNodes[5].firstElementChild.style.display= "block";
+            PRODUCT_MODAL.childNodes[4].childNodes[5].firstElementChild.innerHTML= `<strike>${product.prices.old}</strike>`;//colocación de precio antiguo
+        }
         PRODUCT_MODAL.childNodes[4].childNodes[5].lastElementChild.innerHTML=product.prices.current;//colocación de precio actual
 
         renderModal();
@@ -20,15 +23,16 @@ const openOfferModal = async (e)=>{
         renderModal();
     }
 }
-const closeOfferModal= ()=>{
+const closeProductModal= ()=>{
     PRODUCT_MODAL.classList.remove("modal-visible");
     MODAL_BACKGROUND[0].classList.remove("is-visible")
     HTML_ELEMENT.style.overflow="auto";
     setTimeout(()=>{
-        PRODUCT_MODAL.childNodes[2].firstElementChild.setAttribute("src","images/loading.gif");//colocación de imagen
+        PRODUCT_MODAL.childNodes[2].firstElementChild.setAttribute("src","../images/load.gif");//colocación de imagen
         PRODUCT_MODAL.childNodes[4].childNodes[1].innerHTML= "Cargando...";
         PRODUCT_MODAL.childNodes[4].childNodes[3].innerHTML= "Cargando...";
         PRODUCT_MODAL.childNodes[4].childNodes[5].firstElementChild.firstElementChild.innerHTML= "Cargando...";
+        PRODUCT_MODAL.childNodes[4].childNodes[5].firstElementChild.style.display= "none";
         PRODUCT_MODAL.childNodes[4].childNodes[5].lastElementChild.innerHTML= "Cargando...";
     },300);
     
@@ -41,15 +45,15 @@ const renderModal= ()=>{
             PRODUCT_MODAL.classList.add("modal-visible");
             document.addEventListener("keyup",(e)=>{
                 if(e.key=="Escape"){
-                    closeOfferModal();
+                    closeProductModal();
                 }
             });
 }
 
 
-PRODUCT_MODAL.firstElementChild.addEventListener("click",closeOfferModal);
+PRODUCT_MODAL.firstElementChild.addEventListener("click",closeProductModal);
 
 for (let i = 0; i < PRODUCTS_ARRAY.length; i++) {
     const element = PRODUCTS_ARRAY[i];
-    element.firstElementChild.firstElementChild.addEventListener(("click"),openOfferModal);
+    element.firstElementChild.firstElementChild.addEventListener(("click"),openProductModal);
 }
--- js/render_product.js
+++ js/render_product.js
@@ -2,23 +2,30 @@ const API= "https://mailindistribucionesapi-default-rtdb.firebaseio.com/product/
 var PRODUCTS_ARRAY= document.querySelectorAll(".product");
 
 const renderProduct= async (element)=>{
-    let product_number= element.classList[1];
-    let productPromise = await fetch(`${API}${product_number}.json`);//llamado a la base de datos
-    let product = await productPromise.json();//llamado a la base de datos
-    let oldPrice = parseInt(product.prices.old.replace(".",""));
-    let currentPrice = parseInt(product.prices.current.replace(".",""));
-    let discount= Math.round(( oldPrice - currentPrice ) * 100 / oldPrice);
 
-    //renderizado de producto
-
-    element.firstElementChild.firstElementChild.setAttribute("src", product.images.catalog); //renderizado de imagen
-    if(oldPrice){
-        element.firstElementChild.lastElementChild.style.display="block";
-        element.firstElementChild.lastElementChild.innerHTML= `${discount}%` //renderizado de descuento
-        element.lastElementChild.lastElementChild.firstElementChild.innerHTML= `<strike>${product.prices.old}</strike>` //renderizado de descuento
+    try {
+        let product_number= element.classList[1];
+        let productPromise = await fetch(`${API}${product_number}.json`);//llamado a la base de datos
+        let product = await productPromise.json();//llamado a la base de datos
+        let oldPrice = parseInt(product.prices.old.replace(".",""));
+        let currentPrice = parseInt(product.prices.current.replace(".",""));
+        let discount= Math.round(( oldPrice - currentPrice ) * 100 / oldPrice);
+    
+        //renderizado de producto
+    
+        element.firstElementChild.firstElementChild.setAttribute("src", product.images.catalog); //renderizado de imagen
+        if(product.prices.old){
+            element.firstElementChild.lastElementChild.style.display="block";
+            element.firstElementChild.lastElementChild.innerHTML= `${discount}%` //renderizado de descuento
+            element.lastElementChild.lastElementChild.firstElementChild.style.display="block";
+            element.lastElementChild.lastElementChild.firstElementChild.innerHTML= `<strike>${product.prices.old}</strike>` //renderizado de descuento
+        }
+        element.lastElementChild.firstElementChild.innerHTML= product.name //renderizado de descuento
+        element.lastElementChild.lastElementChild.lastElementChild.innerHTML= product.prices.current //renderizado de descuento
+        
+    } catch (error) {
+        
     }
-    element.lastElementChild.firstElementChild.innerHTML= product.name //renderizado de descuento
-    element.lastElementChild.lastElementChild.lastElementChild.innerHTML= product.prices.current //renderizado de descuento
 
 }
 
