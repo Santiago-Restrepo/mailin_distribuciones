b36af6b5fa3ae9df83d830f1db6761ffb40914c7
Santiago Restrepo <santirstrp@gmail.com>
2021-11-02 19:11:10 -0500

    Glider actualizado, se añade opcion para cuando no hayan ofertas

--- src/scripts/glider.js
+++ src/scripts/glider.js
@@ -1,67 +1,65 @@
+const salesElement = document.getElementById("sales");
+const gliderElement = document.querySelector(".glider");
 const gliderList = document.querySelector('.glider__list');
-const gliderArrowLeft = document.querySelector('.glider__left');
-const gliderArrowRight = document.querySelector('.glider__right');
+const gliderArrowPrev = document.querySelector('.glider-prev');
+const gliderArrowNext = document.querySelector('.glider-next');
 import { client } from '../client';
+
+
 
 
 export const gliderSales = async()=>{
 	try {
 		const salesResponse = await client.getEntries();
-		const sales = salesResponse.items.filter((item) => item.sys.contentType.sys.id === "sales")
-		sales.forEach((sale) =>{
-			const imageElement = document.createElement('img');
-			imageElement.setAttribute('src',`https:${sale.fields.image.fields.file.url}`);
-			imageElement.setAttribute('alt',`imagen de ${sale.fields.title}`);
-			imageElement.setAttribute('loading',`lazy`);
-			gliderList.appendChild(imageElement);
-		})
-		new Glider(document.querySelector('.glider__list'), {
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			draggable: false,
-			duration:2,
-			dots: '.glider__dots',
-			dragVelocity:1,
-			rewind: true,
-			resizeLock: true,
-			arrows: {
-				prev: '.glider-prev',
-				next: '.glider-next'
+		//Filtramos la respuesta del contentful por las ofertas activas
+		const sales = salesResponse.items.filter((item) => item.sys.contentType.sys.id === "sales").filter((sale)=> sale.fields.active)
+		//Si no hay ninguna oferta activa se renderizará "No hay ofertas disponibles"
+		if (sales.length > 0) {
+			gliderElement.classList.remove("hide");
+			//Por cada oferta creamos la imagen, le añadimos el atributo src y hacemos append al gliderList
+			sales.forEach((sale) =>{
+				const imageElement = document.createElement('img');
+				imageElement.setAttribute('src',`https:${sale.fields.image.fields.file.url}`);
+				imageElement.setAttribute('alt',`imagen de ${sale.fields.title}`);
+				imageElement.setAttribute('loading',`lazy`);
+				gliderList.appendChild(imageElement);
+			})
+			//Creación del glider
+			new Glider(document.querySelector('.glider__list'), {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				draggable: false,
+				duration:2,
+				dots: '.glider__dots',
+				dragVelocity:1,
+				rewind: true,
+				resizeLock: true,
+				arrows: {
+					prev: '.glider-prev',
+					next: '.glider-next'
+				}
+			});
+			function stopScroll (){
+				clearInterval(intervalScroll);
 			}
-		});
+			
+			function scrollSlide(){
+				gliderArrowNext.click();
+			}
+			gliderArrowPrev.addEventListener('mouseover',stopScroll);
+			gliderArrowNext.addEventListener('mouseover',stopScroll);
+
+			let intervalScroll = window.setInterval(scrollSlide,2500);
+		} else {
+			//Creación de h5 con contenido de "No hay ofertas disponibles"
+			const noOfferElement = document.createElement("h5");
+			noOfferElement.innerHTML = "No hay ofertas disponibles"
+			salesElement.appendChild(noOfferElement);
+		}
 	} catch (error) {
 		console.error(error);
 	}
 	
 }
-// try {
-	
-	// 	let instagramPromise = await fetch('https://api.instagram.com/oembed/?url=https://www.instagram.com/p/CSs2l7ED_Rs/&access_token=IGQVJYUmVOUkh1M3dEQ1c3akYwTzNvU1VQU05RcVpwVm4tdW5KZAjQxTmRQaE1xSl9RM3RPajBwSndzcmJmUkFtc0VCb05qNnVtMHVUcTNYOEprc1BHbWszRHVWelJLMURXV2tXOGtXSDdHNXhmVmx3UgZDZD',{
-		// 		mode: 'no-cors',
-		// 		access_token:'IGQVJYUmVOUkh1M3dEQ1c3akYwTzNvU1VQU05RcVpwVm4tdW5KZAjQxTmRQaE1xSl9RM3RPajBwSndzcmJmUkFtc0VCb05qNnVtMHVUcTNYOEprc1BHbWszRHVWelJLMURXV2tXOGtXSDdHNXhmVmx3UgZDZD'
-		// 	});
-		// 	debugger
-		// 	let instagramJson = await instagramPromise.json();//Conversión de promesa a JSON
-		// } catch (error) {
-		// 	console.error(error);
 
 
-
-
-// gliderList.addEventListener('scroll',stopScroll);
-
-// gliderArrowLeft.addEventListener('mouseover',stopScroll);
-// gliderArrowRight.addEventListener('mouseover',stopScroll);
-
-
-
-
-// function stopScroll (){
-// 	clearInterval(intervalScroll);
-// }
-
-// function scrollSlide(){
-// 	document.querySelector(".glider__right").click();
-// 	// intervalScroll = window.setInterval(scrollSlide,2500);
-// }
-// let intervalScroll = window.setInterval(scrollSlide,2500);
